64f279d39b8245433aa61b4248ff61ba08d003cc
Joao Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2017-10-18 19:07:25 -0700

    Added numpy include library to gcc

--- openmm/lib/compile_pyx.sh
+++ openmm/lib/compile_pyx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-module load GCC
-module load binutils/2.25
+#module load GCC
+#module load binutils/2.25
 
 if [ "$#" -ne 1 ]
 then
@@ -15,6 +15,7 @@ then
   cython -a $pyxFile
 fi
 
-PYTHON_INCLUDE=$( python -c "from distutils import sysconfig; print sysconfig.get_python_inc()")
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I${PYTHON_INCLUDE} -o ${pyxFile%%.pyx}.so ${pyxFile%%.pyx}.c
+PYTHON_INCLUDE=$( python -c "from distutils import sysconfig; print sysconfig.get_python_inc()" )
+NUMPY_INCLUDE=$( python -c "import numpy; print numpy.get_include()" )
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I${PYTHON_INCLUDE} -I$NUMPY_INCLUDE -o ${pyxFile%%.pyx}.so ${pyxFile%%.pyx}.c
 
