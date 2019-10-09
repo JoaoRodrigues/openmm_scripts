f8ec47388e4f43ca7de464c17066b24b2d2e2fdb
Joao Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2019-10-09 11:32:39 -0700

    Updated forcefield. Added calls to shared code

--- openmm/amberff/minimizeSystem.py
+++ openmm/amberff/minimizeSystem.py
@@ -14,35 +14,18 @@ import os
 import random
 import sys
 
-import numpy as np
-
 import simtk.openmm.app as app
 import simtk.openmm as mm
 import simtk.unit as units
+
+import _utils
+import _restraints
 
 # Format logger
 logging.basicConfig(stream=sys.stdout,
                     level=logging.INFO,
                     format='[%(asctime)s] %(message)s',
                     datefmt='%Y/%m/%d %H:%M:%S')
-
-
-def get_filename(name):
-    """Finds and moves existing file with same name"""
-
-    if not os.path.isfile(name):
-        return name
-
-    rootname = name
-    num = 1
-    while 1:
-        name = '#{}.{}#'.format(rootname, num)
-        if os.path.isfile(name):
-            num += 1
-        else:
-            os.rename(rootname, name)
-            break
-    return rootname
 
 
 ##
@@ -59,14 +42,14 @@ ap.add_argument('--forcefield', type=str, default='amber99sbildn.xml',
 ap.add_argument('--solvent', type=str, default='tip3p.xml',
                 help='Solvent model to use in minimization.')
 ap.add_argument('--platform', type=str, default=None,
-                help='Platform to run calculations on. Defaults to fastest available.')
+                help='Platform to run calculations on.')
 ap.add_argument('--seed', type=int, default=917,
                 help='Seed number for random number generator(s).')
 
-ap.add_argument('--posre', choices=['heavy-atoms'], type=str,
-                help='Add position restraints to a specific group of atoms. Solvent is *never* restrained.')
+ap.add_argument('--posre', action='store_true',
+                help='Add position restraints to non-solvent heavy atoms')
 ap.add_argument('--posre_K', default=1000.0, type=float,
-                help='Force constant for position restraints in kJ.mol/nm^2. Default is 1000.')
+                help='Force constant for position restraints in kJ.mol/nm^2.')
 ap.add_argument('--iterations', default=100, type=int,
                 help='Number of iterations to minimize for. Default is 100.')
 
@@ -75,10 +58,6 @@ cmd = ap.parse_args()
 # Set random seed for reproducibility
 random.seed(cmd.seed)
 
-# Figure out platform
-if cmd.platform is not None:
-    cmd.platform = mm.Platform.getPlatformByName(cmd.platform)
-
 logging.info('Started')
 logging.info('Using:')
 logging.info('  initial structure: {}'.format(cmd.structure))
@@ -86,26 +65,7 @@ logging.info('  force field: {}'.format(cmd.forcefield))
 logging.info('  random seed: {}'.format(cmd.seed))
 
 # Set platform-specific properties
-properties = {}
-if cmd.platform:
-    platform_name = cmd.platform.getName()
-    logging.info('  platform: {}'.format(platform_name))
-
-    if platform_name == 'CUDA':
-        properties = {'CudaPrecision': 'mixed'}
-
-        gpu_ids = os.getenv('CUDA_VISIBLE_DEVICES').split(',')
-        n_gpu = len(gpu_ids)
-        logging.info('  no. of GPUs: {}'.format(n_gpu))
-
-        if gpu_ids:
-            gpu_ids = [str(i) for i, _ in enumerate(gpu_ids)]
-            properties['DeviceIndex'] = ','.join(gpu_ids)
-
-    elif platform_name == 'CPU':
-        cpu_threads = os.getenv('SLURM_CPUS_PER_TASK')
-        if cpu_threads:
-            properties['Threads'] = cpu_threads
+platform, plat_properties = _utils.get_platform(cmd.platform)
 
 # Split input file name
 fname, fext = os.path.splitext(cmd.structure)
@@ -129,35 +89,13 @@ integrator.setConstraintTolerance(0.00001)
 
 # Add position restraints if requested
 if cmd.posre:
-    logging.info('Adding harmonic position restraints on subset: {}'.format(cmd.posre))
-    logging.info('  K = {:8.2f}'.format(cmd.posre_K))
-    # Harmonic restraint
-    posre = mm.CustomExternalForce("0.5*k*periodicdistance(x, y, z, x0, y0, z0)^2")
-    posre.addGlobalParameter("k", cmd.posre_K * (units.kilojoule_per_mole/units.nanometer**2))
-    posre.addPerParticleParameter("x0")
-    posre.addPerParticleParameter("y0")
-    posre.addPerParticleParameter("z0")
-
-    # Define subset of atoms to restraint
-    if cmd.posre == 'heavy-atoms':
-        # Lets assume simple things
-        solvent = set(('HOH', 'NA', 'CL'))
-        _elem_H = app.element.hydrogen
-        all_atoms = list(structure.topology.atoms())
-
-        n_posre_at = 0
-        for i, atom_crd in enumerate(structure.positions):
-            at = all_atoms[i]
-            if at.residue.name not in solvent and at.element != _elem_H:
-                n_posre_at += 1
-                posre.addParticle(i, atom_crd.value_in_unit(units.nanometers))
-
-        system.addForce(posre)
-        logging.info('Restrained {} out of {} atoms'.format(n_posre_at, i))
+    force = _restraints.make_heavy_atom_restraints(structure,
+                                                   cmd.posre_K)
+    system.addForce(force)
 
 # Minimize
 simulation = app.Simulation(structure.topology, system, integrator,
-                            cmd.platform, properties)
+                            platform, plat_properties)
 simulation.context.setPositions(structure.positions)
 
 state = simulation.context.getState(getEnergy=True)
@@ -180,7 +118,7 @@ if cmd.output:
 else:
     _fname = fname + '_EM' + '.cif'
 
-cif_fname = get_filename(_fname)
+cif_fname = _utils.make_fname(_fname)
 logging.info('Writing structure to \'{}\''.format(cif_fname))
 with open(cif_fname, 'w') as handle:
     minimized_positions = state.getPositions()
