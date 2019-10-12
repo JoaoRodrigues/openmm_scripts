b983bae388487334893ea561810aa728eaf0aa86
Joao Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2019-10-12 12:17:05 -0700

    Use _utils modules and rename options

--- openmm/amberff/addMembrane.py
+++ openmm/amberff/addMembrane.py
@@ -20,29 +20,13 @@ import simtk.openmm.app as app
 import simtk.openmm as mm
 import simtk.unit as units
 
+import _utils
+
 # Format logger
 logging.basicConfig(stream=sys.stdout,
                     level=logging.INFO,
                     format='[%(asctime)s] %(message)s',
                     datefmt='%Y/%m/%d %H:%M:%S')
-
-
-def get_filename(name):
-    """Finds and moves existing file with same name"""
-
-    if not os.path.isfile(name):
-        return name
-
-    rootname = name
-    num = 1
-    while 1:
-        name = '#{}.{}#'.format(rootname, num)
-        if os.path.isfile(name):
-            num += 1
-        else:
-            os.rename(rootname, name)
-            break
-    return rootname
 
 
 ##
@@ -51,38 +35,40 @@ ap = argparse.ArgumentParser(description=__doc__)
 
 # Mandatory
 ap.add_argument('structure', help='Input coordinate file (.cif or .pdb)')
+
 # Options
-ap.add_argument('--output', type=str, default=None,
-                help='File name for final system, in mmCIF format.')
-ap.add_argument('--forcefield', type=str, default='amber99sbildn.xml',
-                help='Force field to build the system with.')
-ap.add_argument('--lipid', type=str, default='POPC',
-                help='Lipid type used to build membrane (default: POPC)')
-ap.add_argument('--paddingXY', type=float, default=1.0,
-                help='Padding in XY dimensions (in nm) when adding membrane')
-ap.add_argument('--solvent', type=str, default='tip3p.xml',
-                help='Solvent model.')
-ap.add_argument('--neutralize', action='store_true',
-                help='Adds counter-ions to neutralize total system charge.')
-ap.add_argument('--ionic-strength', type=float, default=0.15,
-                help='Molar concentration of counter-ions. Default is 0.15M.')
-ap.add_argument('--cation', type=str, default='Na+',
-                help='Positive ion used to neutralize system charge.')
-ap.add_argument('--anion', type=str, default='Cl-',
-                help='Negative ion used to neutralize system charge.')
-ap.add_argument('--platform', type=str, default=None,
-                help='Platform to run calculations.')
-ap.add_argument('--seed', type=int, default=917,
+go = ap.add_argument_group('General Options')
+go.add_argument('--output', type=str, default=None,
+                help='File name for solvated system. Will *always* use mmCIF format.')
+go.add_argument('--seed', type=int, default=917,
                 help='Seed number for random number generator(s).')
+
+ff = ap.add_argument_group('Force field Options')
+ff.add_argument('--forcefield', type=str, default='amber14-all.xml',
+                help='Force field to build the system with.')
+ff.add_argument('--solvent', type=str, default='amber14/tip3p.xml',
+                help='Solvent model to use in minimization.')
+
+mem = ap.add_argument_group('Membrane Options')
+mem.add_argument('--lipid', type=str, default='POPC',
+                 help='Lipid type used to build membrane (default: POPC)')
+mem.add_argument('--paddingXY', type=float, default=1.0,
+                 help='Padding in XY dimensions (in nm) when adding membrane')
+mem.add_argument('--neutralize', action='store_true',
+                 help='Adds counter-ions to neutralize total system charge.')
+
+sol = ap.add_argument_group('Solvation Options')
+sol.add_argument('--ionic-strength', type=float, default=0.15,
+                 help='Molar concentration of counter-ions. Default is 0.15M.')
+sol.add_argument('--cation', type=str, default='K+',
+                 help='Positive ion used to neutralize system charge.')
+sol.add_argument('--anion', type=str, default='Cl-',
+                 help='Negative ion used to neutralize system charge.')
 
 cmd = ap.parse_args()
 
 # Set random seed for reproducibility
 random.seed(cmd.seed)
-
-# Figure out platform
-if cmd.platform is not None:
-    cmd.platform = mm.Platform.getPlatformByName(cmd.platform)
 
 logging.info('Started')
 logging.info('Using:')
@@ -97,28 +83,6 @@ if cmd.neutralize:
     logging.info('  cation: {}'.format(cmd.cation))
     logging.info('  anion: {}'.format(cmd.anion))
 logging.info('  random seed: {}'.format(cmd.seed))
-
-# Set platform-specific properties
-properties = {}
-if cmd.platform:
-    platform_name = cmd.platform.getName()
-    logging.info('  platform: {}'.format(platform_name))
-
-    if platform_name == 'CUDA':
-        properties = {'CudaPrecision': 'mixed'}
-
-        gpu_ids = os.getenv('CUDA_VISIBLE_DEVICES').split(',')
-        n_gpu = len(gpu_ids)
-        logging.info('  no. of GPUs: {}'.format(n_gpu))
-
-        if gpu_ids:
-            gpu_ids = [str(i) for i, _ in enumerate(gpu_ids)]
-            properties['DeviceIndex'] = ','.join(gpu_ids)
-
-    elif platform_name == 'CPU':
-        cpu_threads = os.getenv('SLURM_CPUS_PER_TASK')
-        if cpu_threads:
-            properties['Threads'] = cpu_threads
 
 # Figure out input file format from extension
 fname, fext = os.path.splitext(cmd.structure)
@@ -153,7 +117,7 @@ logging.info('  num. lipids     = {:6d} {:4s}s'.format(n_lipid, cmd.lipid))
 logging.info('  num. waters   = {:6d}'.format(n_waters))
 if cmd.neutralize:
     logging.info('  num. ions     = {:6d} {} {:6d} {}'.format(n_cation, cmd.cation,
-                                                              cmd.anion, n_anion))
+                                                              n_anion, cmd.anion))
 
 # Write complete structure
 if cmd.output:
@@ -164,7 +128,7 @@ if cmd.output:
 else:
     _fname = fname + '_withMembrane' + '.cif'
 
-cif_fname = get_filename(_fname)
+cif_fname = _utils.make_fname(_fname)
 logging.info('Writing structure to \'{}\''.format(cif_fname))
 with open(cif_fname, 'w') as handle:
     app.PDBxFile.writeFile(modeller.topology, modeller.positions, handle, keepIds=True)
