22afa60952856430e8caa99a86ea553d0e3cf860
João Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2019-10-21 19:00:41 -0700

    Refactored script to look nicer and read nicer

--- utils/trimBox.py
+++ utils/trimBox.py
@@ -4,7 +4,7 @@
 Trims a periodic box to fit to certain dimensions.
 
 Calculates solute (protein) center of mass and defines distances
-from that point.
+from that point. Structure must NOT be split between different periodic images.
 
 .2019. joaor@stanford.edu
 """
@@ -14,7 +14,6 @@ from __future__ import print_function, division
 import argparse
 import logging
 import os
-import random
 import sys
 
 import numpy as np
@@ -58,38 +57,39 @@ ap.add_argument('--output', type=str, default=None,
                 help='File name for final system, in mmCIF format.')
 ap.add_argument('--solute', type=str, nargs='+',
                 help='Solute chain(s)')
-x_opts = ap.add_mutually_exclusive_group()
-x_opts.add_argument('--distanceX', type=float,
-                    help='Distance (in nm) from c.o.m. to keep atoms (x)')
-x_opts.add_argument('--paddingX', type=float,
-                    help='Distance (in nm) around protein to keep atoms (x)')
-y_opts = ap.add_mutually_exclusive_group()
-y_opts.add_argument('--distanceY', type=float,
-                    help='Distance (in nm) from c.o.m. to keep atoms (y)')
-y_opts.add_argument('--paddingY', type=float,
-                    help='Distance (in nm) around protein to keep atoms (y)')
+xy_opts = ap.add_mutually_exclusive_group()
+xy_opts.add_argument('--distance-XY', type=float, dest='distXY',
+                     help=('Distance in the XY plane (in nm) from the c.o.m of'
+                           ' the solute to keep atoms in the box.'))
+xy_opts.add_argument('--padding-XY', type=float, dest='padXY',
+                     help=('Distance in the XY plane (in nm) from the edge of'
+                           ' the solute to keep atoms in the box.'))
 z_opts = ap.add_mutually_exclusive_group()
-z_opts.add_argument('--distanceZ', type=float,
-                    help='Distance (in nm) from c.o.m. to keep atoms (z)')
-z_opts.add_argument('--paddingZ', type=float,
-                    help='Distance (in nm) around protein to keep atoms (z)')
+z_opts.add_argument('--distance-Z', type=float, dest='distZ',
+                    help=('Distance along Z axis (in nm) from c.o.m of the'
+                          ' solute to keep atoms in the box.'))
+z_opts.add_argument('--padding-Z', type=float, dest='padZ',
+                    help=('Distance along Z axis (in nm) from c.o.m of the'
+                          ' solute to keep atoms in the box.'))
 
 cmd = ap.parse_args()
 
-if not any([cmd.paddingX, cmd.paddingY, cmd.paddingZ,
-            cmd.distanceX, cmd.distanceY, cmd.distanceZ]):
-    logging.error('--padding or --distance must be provided')
+if not any([cmd.distXY, cmd.padXY, cmd.distZ, cmd.padZ]):
+    emsg = 'You must provide at least one --padding or --distance option'
+    logging.error(emsg)
     sys.exit(1)
 
 logging.info('Started')
 logging.info('Using:')
 logging.info('  initial structure: {}'.format(cmd.structure))
-logging.info('  distance X: {}'.format(cmd.distanceX))
-logging.info('  distance Y: {}'.format(cmd.distanceY))
-logging.info('  distance Z: {}'.format(cmd.distanceZ))
-logging.info('  padding X: {}'.format(cmd.paddingX))
-logging.info('  padding Y: {}'.format(cmd.paddingY))
-logging.info('  padding Z: {}'.format(cmd.paddingZ))
+if cmd.distXY:
+    logging.info('  distance XY: {}'.format(cmd.distXY))
+elif cmd.padXY:
+    logging.info('  padding XY: {}'.format(cmd.padXY))
+if cmd.distZ:
+    logging.info('  distance Z: {}'.format(cmd.distanceZ))
+elif cmd.padZ:
+    logging.info('  padding Z: {}'.format(cmd.paddingZ))
 
 # Figure out input file format from extension
 fname, fext = os.path.splitext(cmd.structure)
@@ -98,15 +98,14 @@ fname, fext = os.path.splitext(cmd.structure)
 structure = app.PDBxFile(cmd.structure)
 modeller = app.Modeller(structure.topology, structure.positions)
 
-# Identify protein atoms
-if cmd.solute:
+# Identify solute atoms
+if cmd.solute:  # user provided
     solute_ids = sorted(cmd.solute)
-else:
-    wat = set(('HOH', 'WAT'))
-    # Ignore water, everyone else is solute
+else:  # automatically from residue names
+    non_solute = set(('HOH', 'WAT', 'K', 'NA', 'CL', 'POP'))
     solute_ids = set()
     for residue in modeller.topology.residues():
-        if residue.name not in wat:
+        if residue.name not in non_solute:
             solute_ids.add(residue.chain.id)
     solute_ids = sorted(solute_ids)
 
@@ -115,130 +114,105 @@ logging.info('Solute chains: {}'.format(solute_ids))
 # Get center of mass of solute
 _xyz = [(x._value, y._value, z._value) for x, y, z in modeller.positions]
 xyz = np.array(_xyz, dtype=np.float)
-others_idx, solute_idx = [], []
+solute_idx = []
 for chain in modeller.topology.chains():
     if chain.id not in solute_ids:
-        idx_list = others_idx
-    else:
-        idx_list = solute_idx
+        continue
+    idx_list = solute_idx
     for atom in chain.atoms():
         idx_list.append(atom.index)
 
 if not solute_idx:
-    raise ValueError('Chains not found in structure.')
+    logging.error('Solute chains not found in structure.')
+    sys.exit(1)
 
 solute_com = xyz[solute_idx, :].mean(axis=0)
+logging.info(f'Solute center of mass: {solute_com}')
+
+# Get original system and solute dimensions/unit cell vectors
+sys_natoms = len(xyz)
+ssizeX, ssizeY, ssizeZ = xyz.max(axis=0) - xyz.min(axis=0)
+logging.info(f'System size: x={ssizeX:6.3f} y={ssizeY:6.3f} z={ssizeZ:6.3f}')
+
 solute_xyz = xyz[solute_idx, :]
 sizeX, sizeY, sizeZ = solute_xyz.max(axis=0) - solute_xyz.min(axis=0)
-
-# If lipids, check which are on which lipids
-resMeanZ = {}
-membraneMeanZ = 0.0
-numLipidAtoms = 0
-for res in modeller.topology.residues():
-    if res.name.startswith('POP'):
-        numResAtoms = 0
-        sumZ = 0.0
-        for atom in res.atoms():
-            numResAtoms += 1
-            sumZ += xyz[atom.index][2]
-        numLipidAtoms += numResAtoms
-        membraneMeanZ += sumZ
-        resMeanZ[res] = sumZ/numResAtoms
-if numLipidAtoms:
-    membraneMeanZ /= numLipidAtoms
-    lipidLeaf = dict((res, 0 if resMeanZ[res] < membraneMeanZ else 1) for res in resMeanZ)
-else:
-    lipidLeaf = {}
-logging.info('Lipid Molecules: {}'.format(len(lipidLeaf)))
+logging.info(f'Solute size: x={sizeX:6.3f} y={sizeY:6.3f} z={sizeZ:6.3f}')
+vX, vY, vZ = modeller.topology.getUnitCellDimensions().value_in_unit(units.nanometer)
+logging.info(f'Box dimensions: x={vX:6.3f} y={vY:6.3f} z={vZ:6.3f}')
+og_volume = vX * vY * vZ
+logging.info(f'Box volume: {og_volume:6.3f}')
 
 # Calculate boundaries
-if cmd.distanceX:
-    max_dx = cmd.distanceX + solute_com[0]
-    min_dx = solute_com[0] - cmd.distanceX
-if cmd.distanceY:
-    max_dy = cmd.distanceY + solute_com[1]
-    min_dy = solute_com[1] - cmd.distanceY
-if cmd.paddingX:
-    max_dx = cmd.paddingX + solute_com[0] + sizeX/2
-    min_dx = solute_com[0] - sizeX/2 - cmd.paddingX
-if cmd.paddingY:
-    max_dy = cmd.paddingY + solute_com[1] + sizeY/2
-    min_dy = solute_com[1] - sizeY/2 - cmd.paddingY
-if cmd.distanceZ:
-    max_dz = cmd.distanceZ + solute_com[2]
-    min_dz = solute_com[2] - cmd.distanceZ
-if cmd.paddingZ:
-    max_dz = cmd.paddingZ + solute_com[2] + sizeZ/2
-    min_dz = solute_com[2] - sizeZ/2 - cmd.paddingZ
+if cmd.distXY:
+    max_dx = cmd.distXY + solute_com[0]
+    min_dx = solute_com[0] - cmd.distXY
+    max_dy = cmd.distXY + solute_com[1]
+    min_dy = solute_com[1] - cmd.distXY
+elif cmd.padXY:
+    max_dx = cmd.padXY + solute_com[0] + (sizeX / 2)
+    min_dx = solute_com[0] - (sizeX / 2) - cmd.padXY
+    max_dy = cmd.padXY + solute_com[1] + (sizeY / 2)
+    min_dy = solute_com[1] - (sizeY / 2) - cmd.padXY
 
-# Pick outside atoms
+if cmd.distZ:
+    max_dz = cmd.distZ + solute_com[2]
+    min_dz = solute_com[2] - cmd.distZ
+elif cmd.padZ:
+    max_dz = cmd.padZ + solute_com[2] + (sizeZ / 2)
+    min_dz = solute_com[2] - (sizeZ / 2) - cmd.padZ
+
+# Identify atoms to be deleted
 idx = []
-if cmd.distanceX or cmd.paddingX:
+if cmd.distXY or cmd.padXY:
     x = xyz[:, 0]
-    idx += np.where(np.logical_or(x < min_dx, x > max_dx))[0].tolist()
-if cmd.distanceY or cmd.paddingY:
     y = xyz[:, 1]
+    idx += np.where(np.logical_or(x < min_dx, x > max_dx))[0].tolist()
     idx += np.where(np.logical_or(y < min_dy, y > max_dy))[0].tolist()
-if cmd.distanceZ or cmd.paddingZ:
+if cmd.distZ or cmd.padZ:
     z = xyz[:, 2]
     idx += np.where(np.logical_or(z < min_dz, z > max_dz))[0].tolist()
 
 # Delete flagged non-solute residues (that have atoms outside the boundaries)
 # To avoid cutting an entire residue if only one atom is outside the boundary
 # use a % criterion
-_FRACTION = 0.70  # was 0.98
+_FRACTION = 0.70
 idx = set(idx) - set(solute_idx)
-atoms = []
+to_remove = []
 for residue in modeller.topology.residues():
     atom_idx = {a.index for a in residue.atoms()}
     fraction = len(atom_idx & idx) / len(atom_idx)
     if fraction > _FRACTION:
-        # print(residue, fraction, len(atom_idx & idx))
-        atoms += list(residue.atoms())
-logging.info('Deleting {} atoms'.format(len(atoms)))
-modeller.delete(atoms)
+        to_remove += list(residue.atoms())
+n_removed = len(to_remove)
+modeller.delete(to_remove)
 
-# Set unit cell dimensions
-# Only change vectors that we modified
-logging.info('Adjusting unit cell dimensions:')
-vX, vY, vZ = modeller.topology.getUnitCellDimensions()
+logging.info((f'Deleted {n_removed} atoms:'
+             f' {n_removed*100 / sys_natoms:6.3f} % of total'))
+
+# Adjust unit cell dimensions
 _xyz = [(x._value, y._value, z._value) for x, y, z in modeller.positions]
 xyz = np.array(_xyz, dtype=np.float)
-# boxX, boxY, boxZ = xyz.max(axis=0) - solute_xyz.min(axis=0)
 
-if cmd.distanceX or cmd.paddingX:
-    if cmd.distanceX:
-        pad = cmd.distanceX
-    else:
-        pad = cmd.paddingX
-    vX = (sizeX+2*pad)
-    # vX = boxX
-    logging.info('  vx: {}'.format(vX))
-else:
-    vX = vX.value_in_unit(units.nanometer)
-if cmd.distanceY or cmd.paddingY:
-    if cmd.distanceY:
-        pad = cmd.distanceY
-    else:
-        pad = cmd.paddingY
-    vY = (sizeY+2*pad)
-    # vY = boxY
-    logging.info('  vy: {}'.format(vY))
-else:
-    vY = vY.value_in_unit(units.nanometer)
-if cmd.distanceZ or cmd.paddingZ:
-    if cmd.distanceZ:
-        pad = cmd.distanceZ
-    else:
-        pad = cmd.paddingZ
-    vZ = (sizeZ+2*pad)
-    # vZ = boxZ
-    logging.info('  vz: {}'.format(vZ))
-else:
-    vZ = vZ.value_in_unit(units.nanometer)
+if cmd.distXY or cmd.padXY:
+    logging.info('Adjusting XY dimensions')
+    offset = cmd.distXY if cmd.distXY is not None else cmd.padXY
+    vX = (sizeX + 2 * offset)
+    vY = (sizeY + 2 * offset)
+
+if cmd.distZ or cmd.padZ:
+    logging.info('Adjusting Z dimensions')
+    offset = cmd.distZ if cmd.distZ is not None else cmd.padZ
+    vZ = (sizeZ + 2 * offset)
 
 modeller.topology.setUnitCellDimensions((vX, vY, vZ))
+
+# Print some stats to make user happy
+new_volume = vX * vY * vZ
+vol_ratio = new_volume / og_volume
+vX, vY, vZ = modeller.topology.getUnitCellDimensions().value_in_unit(units.nanometer)
+
+logging.info(f'Trimmed box dimensions: x={vX:6.3f} y={vY:6.3f} z={vZ:6.3f}')
+logging.info(f'Trimmed/Initial Volume Ratio: {vol_ratio:6.3f}')
 
 # Write complete structure
 if cmd.output:
@@ -252,4 +226,5 @@ else:
 cif_fname = get_filename(_fname)
 logging.info('Writing structure to \'{}\''.format(cif_fname))
 with open(cif_fname, 'w') as handle:
-    app.PDBxFile.writeFile(modeller.topology, modeller.positions, handle, keepIds=True)
+    app.PDBxFile.writeFile(modeller.topology, modeller.positions,
+                           handle, keepIds=True)
